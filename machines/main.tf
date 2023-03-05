bfa8a886713e8742a9d9cfbd174d4b1f0f37eb6e
Corentin Pape <corentin.pape@epita.fr>
2023-03-05 19:35:39 +0100

    .

--- machines/main.tf
+++ machines/main.tf
@@ -1,7 +1,8 @@
-# -----------------------------------------------------------------------------
-# Vultr machine
 module "vultr_machine" {
   source       = "./vultr"
   dyndns_token = var.dyndns_token
 }
-# -----------------------------------------------------------------------------
+module "raspipcgamer_machine" {
+  source       = "./raspipcgamer"
+  dyndns_token = var.dyndns_token
+}
