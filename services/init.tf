142e8d81c078ef1f6a23197d3feab5dc6338def0
Corentin Pape <corentin.pape@epita.fr>
2023-03-09 00:06:01 +0100

    feat(s3_backend): use backend for machines and services

--- services/init.tf
+++ services/init.tf
@@ -1,4 +1,11 @@
 terraform {
+  backend "s3" {
+    bucket         = "cocopaps-terraform-states"
+    key            = "infra/services.tf"
+    region = "eu-west-3"
+    dynamodb_table = "cocopaps-terraform-locks"
+    encrypt        = true
+  }
   required_providers {
     docker = {
       source  = "kreuzwerker/docker"
