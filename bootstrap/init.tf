86fc44beb8fcad3afdabd9785940075e0dd114eb
Corentin Pape <corentin.pape@epita.fr>
2023-03-09 00:05:42 +0100

    feat(s3_backend): add bootstrap configuration (s3 iam user, bucket and dynamodb)

---
+++ bootstrap/init.tf
@@ -0,0 +1,19 @@
+terraform {
+  backend "s3" {
+    bucket         = "cocopaps-terraform-states"
+    key            = "infra/bootstrap.tf"
+    region = "eu-west-3"
+    dynamodb_table = "cocopaps-terraform-locks"
+    encrypt        = true
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-3"
+}
