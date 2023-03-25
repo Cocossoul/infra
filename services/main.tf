f4bcac9263e8a42794abe9c9dbd18f854c11e89b
Corentin Pape <corentin.pape@epita.fr>
2023-03-25 11:45:52 +0100

    chore(services): disable minecraft server, waiting for rcon and launch

--- services/main.tf
+++ services/main.tf
@@ -35,14 +35,14 @@ module "home" {
   }
 }
 
-module "minecraft_server" {
-  source                = "./minecraft_server"
-  domain_name           = data.cloudflare_zone.cocopaps.name
-  domain_zone_id        = data.cloudflare_zone.cocopaps.zone_id
-  subdomain             = "mc"
-  machine_name          = local.homeserver_machine.name
-  machine_dyndns_domain = local.homeserver_machine.dyndns_domain
-  providers = {
-    docker = docker.homeserver_machine
-  }
-}
+# module "minecraft_server" {
+#   source                = "./minecraft_server"
+#   domain_name           = data.cloudflare_zone.cocopaps.name
+#   domain_zone_id        = data.cloudflare_zone.cocopaps.zone_id
+#   subdomain             = "mc"
+#   machine_name          = local.homeserver_machine.name
+#   machine_dyndns_domain = local.homeserver_machine.dyndns_domain
+#   providers = {
+#     docker = docker.homeserver_machine
+#   }
+# }
