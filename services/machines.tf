702dc58f84159d440266cfa7e49fa5f5874b2fc7
Corentin Pape <corentin.pape@epita.fr>
2023-03-30 17:04:48 +0200

    feat(raspipcgamer): add netdata

--- services/machines.tf
+++ services/machines.tf
@@ -42,6 +42,8 @@ locals {
   raspipcgamer_machine = {
     dyndns_domain = data.terraform_remote_state.machines.outputs.raspipcgamer_dyndns_domain
     name          = "raspipcgamer"
+    domain     = "raspi.${ data.cloudflare_zone.cocopaps.name }"
+    local_network_ip = "192.168.1.23"
   }
 }
 resource "cloudflare_record" "broker" {
@@ -50,6 +52,34 @@ resource "cloudflare_record" "broker" {
   value   = local.raspipcgamer_machine.dyndns_domain
   type    = "CNAME"
   ttl     = 3600
+}
+resource "cloudflare_record" "raspi" {
+  zone_id = data.cloudflare_zone.cocopaps.zone_id
+  name    = "raspi"
+  value   = local.raspipcgamer_machine.dyndns_domain
+  type    = "CNAME"
+  ttl     = 3600
+}
+provider "docker" {
+  host     = "ssh://pi@${local.raspipcgamer_machine.dyndns_domain}:1882"
+  ssh_opts = ["-o", "StrictHostKeyChecking=no", "-o", "UserKnownHostsFile=/dev/null"]
+  alias    = "raspipcgamer_machine"
+}
+module "raspipcpgamer_reverse-proxy" {
+  source = "./reverse-proxy-insecure"
+  providers = {
+    docker = docker.raspipcgamer_machine
+  }
+}
+module "raspipcgamer_netdata" {
+  source    = "./netdata"
+  domain    = data.cloudflare_zone.cocopaps
+  machine   = local.raspipcgamer_machine
+  subdomain = "monitoring.raspi"
+  entrypoint = "insecure"
+  providers = {
+    docker = docker.raspipcgamer_machine
+  }
 }
 # -----------------------------------------------------------------------------
 locals {
@@ -74,6 +104,17 @@ module "homeserver_netdata" {
   domain    = data.cloudflare_zone.cocopaps
   machine   = local.homeserver_machine
   subdomain = "monitoring.homeserver"
+  providers = {
+    docker = docker.homeserver_machine
+  }
+}
+module "homeserver_bridge_to_raspi" {
+  source  = "./reverse-proxy-internal-bridge"
+  domain = "*.${ local.raspipcgamer_machine.domain }"
+  destination_machine = {
+    name = local.raspipcgamer_machine.name
+    ip = local.raspipcgamer_machine.local_network_ip
+  }
   providers = {
     docker = docker.homeserver_machine
   }
