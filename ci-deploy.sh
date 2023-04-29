4882383291e8e55eb9ed4f65676330fc1f4bcf24
Corentin Pape <corentin.pape@epita.fr>
2023-04-29 19:24:30 +0200

    fix(pipeline): force ssh dir to be /root/.ssh

--- ci-deploy.sh
+++ ci-deploy.sh
@@ -4,25 +4,21 @@ set -e
 set -v
 
 # Configure SSH
-echo "Configure SSH"
-mkdir -p ~/.ssh
-SSH_KEY_PATH=~/.ssh/ssh_key
+mkdir -p "/root/.ssh"
+SSH_KEY_PATH="/root/.ssh/ssh_key"
 printf "%s" "$SSH_CI_PRIVATEKEY_BASE64" | base64 -d > "$SSH_KEY_PATH"
 chmod 0400 "$SSH_KEY_PATH"
-echo "IdentityFile $SSH_KEY_PATH" >> ~/.ssh/config
+echo "IdentityFile $SSH_KEY_PATH" >> "/root/.ssh/config"
 
 # Configure Passbolt CLI
-echo "Configure Passbolt CLI"
 printf "%s" "$PASSBOLT_CI_ACS_BASE64" | base64 -d > passbolt_ci_user.acs
 go-passbolt-cli configure --serverAddress https://passbolt.cocopaps.com --userPassword "$PASSBOLT_CI_PASSWORD" --userPrivateKeyFile 'passbolt_ci_user.acs'
 
 # Fetch secrets from Passbolt
-echo "Fetch secrets from Passbolt"
 source fetch_secrets.sh
 
 # Docker login
-echo "Login to Dockerhub"
 docker login -u cocopaps -p "$DOCKER_PASSWORD"
 
-echo "Start deployment"
+# Start deployment
 ./deploy.sh -auto-approve
