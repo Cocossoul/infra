a2ae25a44b429a8c9abf16bc6c4926d9cef4460d
Corentin Pape <corentin.pape@epita.fr>
2023-05-01 11:26:20 +0200

    feat(wol): add rwol service

--- machines/variables.tf
+++ machines/variables.tf
@@ -1,6 +1,3 @@
 variable "dyndns_token" {
   type = string
 }
-variable "gamerpc_mac_address" {
-  type = string
-}
--- services/variables.tf
+++ services/variables.tf
@@ -1,15 +1,34 @@
 variable "cloudflare_token" {
   type = string
+  sensitve = true
 }
 variable "owncloud_admin_username" {
   type = string
+  sensitve = true
 }
 variable "owncloud_admin_password" {
   type = string
+  sensitve = true
 }
-variable "owncloud_db_password" {}
+variable "owncloud_db_password" {
+  sensitve = true
+}
 variable "rcon_password" {
   type = string
+  sensitve = true
 }
-variable "discord_webhook_homeserver" {}
-variable "discord_webhook_vultr" {}
+variable "gamerpc_mac_address" {
+  sensitve = true
+}
+variable "gamerpc_ip_address" {
+  sensitive = true
+}
+variable "rwol_password" {
+  sensitve = true
+}
+variable "discord_webhook_homeserver" {
+  sensitve = true
+}
+variable "discord_webhook_vultr" {
+  sensitve = true
+}
