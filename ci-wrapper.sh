df497fc3996f2493b3d828b4b0b6c56d15e10151
Corentin Pape <corentin.pape@epita.fr>
2023-05-12 15:15:06 +0200

    feat(pipeline): rename deployment scripts and add terraform plan check for prs

--- ci-wrapper.sh
+++ ci-wrapper.sh
@@ -13,6 +13,7 @@ echo "IdentityFile $SSH_KEY_PATH" >> "/root/.ssh/config"
 # Configure Passbolt CLI
 printf "%s" "$PASSBOLT_CI_ACS_BASE64" | base64 -d > passbolt_ci_user.acs
 go-passbolt-cli configure --serverAddress https://passbolt.cocopaps.com --userPassword "$PASSBOLT_CI_PASSWORD" --userPrivateKeyFile 'passbolt_ci_user.acs'
+rm passbolt_ci_user.acs
 
 # Fetch secrets from Passbolt
 source fetch_secrets.sh
@@ -21,7 +22,4 @@ source fetch_secrets.sh
 docker login -u cocopaps -p "$DOCKER_PASSWORD"
 
 # Start deployment
-./deploy.sh -auto-approve
-
-# Cleanup after deployment
-rm passbolt_ci_user.acs
+./terraform.sh $@
