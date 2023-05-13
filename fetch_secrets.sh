73887da087f6fc46fcfb84ff964a7d82e968dc4e
Corentin Pape <corentin.pape@epita.fr>
2023-05-13 15:32:23 +0200

    fix(fetch_secrets): return an error if failed to fetch a secret

--- fetch_secrets.sh
+++ fetch_secrets.sh
@@ -11,16 +11,41 @@ fetch_secret() {
     echo ${PASSWORD}
 }
 
-export TF_VAR_cloudflare_token="$(fetch_secret cloudflare_token)"
-export VULTR_API_KEY="$(fetch_secret vultr_api_key)"
-export TF_VAR_dyndns_token="$(fetch_secret dyndns_token)"
-export TF_VAR_owncloud_admin_username="$(fetch_secret owncloud_admin_username)"
-export TF_VAR_owncloud_admin_password="$(fetch_secret owncloud_admin_password)"
-export TF_VAR_owncloud_db_password="$(fetch_secret owncloud_db_password)"
-export AWS_ACCESS_KEY_ID="$(fetch_secret AWS_ACCESS_KEY_ID)"
-export AWS_SECRET_ACCESS_KEY="$(fetch_secret AWS_SECRET_ACCESS_KEY)"
-export TF_VAR_gamerpc_mac_address="$(fetch_secret gamerpc_mac_address)"
-export TF_VAR_rcon_password="$(fetch_secret rcon_password)"
-export TF_VAR_discord_webhook_vultr="$(fetch_secret discord_webhook_vultr)"
-export TF_VAR_discord_webhook_homeserver="$(fetch_secret discord_webhook_homeserver)"
-export DOCKER_PASSWORD="$(fetch_secret DOCKER_PASSWORD)"
+TF_VAR_cloudflare_token="$(fetch_secret cloudflare_token)"
+export TF_VAR_cloudflare_token
+
+VULTR_API_KEY="$(fetch_secret vultr_api_key)"
+export VULTR_API_KEY
+
+TF_VAR_dyndns_token="$(fetch_secret dyndns_token)"
+export TF_VAR_dyndns_token
+
+TF_VAR_owncloud_admin_username="$(fetch_secret owncloud_admin_username)"
+export TF_VAR_owncloud_admin_username
+
+TF_VAR_owncloud_admin_password="$(fetch_secret owncloud_admin_password)"
+export TF_VAR_owncloud_admin_password
+
+TF_VAR_owncloud_db_password="$(fetch_secret owncloud_db_password)"
+export TF_VAR_owncloud_db_password
+
+AWS_ACCESS_KEY_ID="$(fetch_secret AWS_ACCESS_KEY_ID)"
+export AWS_ACCESS_KEY_ID
+
+AWS_SECRET_ACCESS_KEY="$(fetch_secret AWS_SECRET_ACCESS_KEY)"
+export AWS_SECRET_ACCESS_KEY
+
+TF_VAR_gamerpc_mac_address="$(fetch_secret gamerpc_mac_address)"
+export TF_VAR_gamerpc_mac_address
+
+TF_VAR_rcon_password="$(fetch_secret rcon_password)"
+export TF_VAR_rcon_password
+
+TF_VAR_discord_webhook_vultr="$(fetch_secret discord_webhook_vultr)"
+export TF_VAR_discord_webhook_vultr
+
+TF_VAR_discord_webhook_homeserver="$(fetch_secret discord_webhook_homeserver)"
+export TF_VAR_discord_webhook_homeserver
+
+DOCKER_PASSWORD="$(fetch_secret DOCKER_PASSWORD)"
+export DOCKER_PASSWORD
