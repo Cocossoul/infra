8d63073624b330a5178cb85c4dce28f590a8d5f3
Corentin Pape <corentin.pape@epita.fr>
2023-08-07 17:11:49 +0200

    fixup! fixup! feat(netdata): add basic auth for netdata

--- services/netdata/variables.tf
+++ services/netdata/variables.tf
@@ -4,4 +4,6 @@ variable "subdomain" {
 }
 variable "machine" {}
 variable "discord_notification_settings" {}
-variable "monitoring_admin_password_hash" {}
+variable "monitoring_admin_password_hash" {
+  sensitive = true
+}
