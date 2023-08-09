5c6655fe7f0ab0c975b0661eb788b35018634ae9
Corentin Pape <corentin.pape@epita.fr>
2023-08-09 16:52:16 +0200

    feat(nightly_maintenance): add some logs

--- services/nightly_maintenance/src/config/task.template.sh
+++ services/nightly_maintenance/src/config/task.template.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+echo "Sending request to Github to run deploy workflow"
+
 curl -H "Accept: application/vnd.github+json" \
     -H "Authorization: token ${ deploy_workflow_token }" \
     --request POST \
