54ac5858c39560db207424c914cd7a3bea4b891b
Corentin Pape <corentin.pape@epita.fr>
2023-08-09 16:45:49 +0200

    feat(nightly_maintenance): change name of dispatch

--- services/nightly_maintenance/src/config/task.template.sh
+++ services/nightly_maintenance/src/config/task.template.sh
@@ -3,5 +3,5 @@
 curl -H "Accept: application/vnd.github+json" \
     -H "Authorization: token ${ deploy_workflow_token }" \
     --request POST \
-    --data '{"event_type": "deploy"}' \
+    --data '{"event_type": "nightly_maintenance_deploy"}' \
     https://api.github.com/repos/Cocossoul/infra/dispatches
