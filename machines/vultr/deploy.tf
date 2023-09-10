e2f3282e82fe3dd67b02e6fe1e0cd852d4f528e3
Corentin Pape <corentin.pape@epita.fr>
2023-09-10 14:40:54 +0200

    feat(vultr): prevent destroy

--- machines/vultr/deploy.tf
+++ machines/vultr/deploy.tf
@@ -13,6 +13,10 @@ resource "vultr_instance" "vultr_machine" {
   enable_ipv6 = false
   ssh_key_ids = ["60f8f596-14d5-4d06-a662-51ce92f4adf3"]
   script_id   = vultr_startup_script.startup_script.id
+
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
 resource "vultr_startup_script" "startup_script" {
