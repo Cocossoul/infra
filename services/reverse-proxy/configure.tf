7b8c0d7acf800b290eb2e00e911c070e527d10be
Corentin Pape <corentin.pape@epita.fr>
2023-09-30 15:56:37 +0200

    fix(fail2ban): don't 64encode 2 times

--- services/reverse-proxy/configure.tf
+++ services/reverse-proxy/configure.tf
@@ -1,7 +1,7 @@
 locals {
-    fail2ban_cloudflare_action = base64encode(templatefile("${path.module}/src/fail2ban_cloudflare.template.action",
+    fail2ban_cloudflare_action = templatefile("${path.module}/src/fail2ban_cloudflare.template.action",
     {
       cloudflare_global_api_key = var.cloudflare_global_api_key
     }
-  ))
+  )
 }
