6cb22097edf4174e1f7a3a82bd9da996400bbf9d
Corentin Pape <corentin.pape@epita.fr>
2023-10-23 22:59:30 +0200

    fixup! Revert "feat: immich use traefik proxy (#225)"

--- services/immich/proxy.tf
+++ services/immich/proxy.tf
@@ -11,15 +11,6 @@ terraform {
   }
 }
 
-resource "cloudflare_record" "immich_proxy" {
-  zone_id = var.domain.zone_id
-  name    = var.subdomain
-  value   = var.machine.address
-  type    = "CNAME"
-  ttl     = 1
-  proxied = true
-}
-
 data "docker_registry_image" "immich_proxy" {
   name = "ghcr.io/immich-app/immich-proxy:v1.82.0" # renovate_docker
 }
