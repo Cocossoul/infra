a6d124f8cac39a991725a923836db9a54d3f12ca
Corentin Pape <corentin.pape@epita.fr>
2023-11-06 22:05:32 +0100

    fix: transform set into list

--- services/dns_domains.tf
+++ services/dns_domains.tf
@@ -11,90 +11,95 @@ locals {
       {
         "subdomain" = "@",
         "private" = false,
-        "domain" = cloudflare_zone.tbeteouquoi
+        "domain" = data.cloudflare_zone.tbeteouquoi
       },
       {
         "subdomain" = "passbolt",
         "private" = false,
-        "domain" = cloudflare_zone.cocopaps
+        "domain" = data.cloudflare_zone.cocopaps
       },
       {
         "subdomain" = "cloud",
         "private" = true,
-        "domain" = cloudflare_zone.cocopaps
+        "domain" = data.cloudflare_zone.cocopaps
       },
       {
         "subdomain" = "gatus",
         "private" = false,
-        "domain" = cloudflare_zone.cocopaps
+        "domain" = data.cloudflare_zone.cocopaps
       },
       {
         "subdomain" = "monitoring",
         "private" = false,
-        "domain" = cloudflare_zone.cocopaps
+        "domain" = data.cloudflare_zone.cocopaps
       },
       {
         "subdomain" = "mealie",
         "private" = true,
-        "domain" = cloudflare_zone.cocopaps
+        "domain" = data.cloudflare_zone.cocopaps
       },
       {
         "subdomain" = "@",
         "private" = false,
-        "domain" = cloudflare_zone.cocopaps
+        "domain" = data.cloudflare_zone.cocopaps
       },
       {
         "subdomain" = "home",
         "private" = false,
-        "domain" = cloudflare_zone.cocopaps
+        "domain" = data.cloudflare_zone.cocopaps
       },
       {
         "subdomain" = "commander",
         "private" = true,
-        "domain" = cloudflare_zone.cocopaps
+        "domain" = data.cloudflare_zone.cocopaps
       },
       {
         "subdomain" = "pdf",
         "private" = false,
-        "domain" = cloudflare_zone.cocopaps
+        "domain" = data.cloudflare_zone.cocopaps
       },
       {
         "subdomain" = "boinc",
         "private" = false,
-        "domain" = cloudflare_zone.cocopaps
+        "domain" = data.cloudflare_zone.cocopaps
       },
       {
         "subdomain" = "firefly",
         "private" = true,
-        "domain" = cloudflare_zone.cocopaps
+        "domain" = data.cloudflare_zone.cocopaps
       },
       {
         "subdomain" = "fireflyimporter",
         "private" = true,
-        "domain" = cloudflare_zone.cocopaps
+        "domain" = data.cloudflare_zone.cocopaps
       },
       {
         "subdomain" = "photos",
         "private" = true,
-        "domain" = cloudflare_zone.cocopaps
+        "domain" = data.cloudflare_zone.cocopaps
       },
       {
         "subdomain" = "n8n"
         "private" = false,
-        "domain" = cloudflare_zone.cocopaps
+        "domain" = data.cloudflare_zone.cocopaps
       },
       {
         "subdomain" = "dns"
         "private" = true,
-        "domain" = cloudflare_zone.cocopaps
+        "domain" = data.cloudflare_zone.cocopaps
       }
     ]
+
+    hostnames_map = {
+        for index, hostname in local.hostnames:
+        "${hostname.subdomain}.${hostname.domain.name}" => hostname
+    }
 }
 
 resource "cloudflare_record" "services" {
-  for_each = local.hostnames
-  zone_id = each.domain.zone_id
-  name    = each.subdomain
+  for_each = local.hostnames_map
+  zone_id = each.value.domain.zone_id
+  name    = each.value.subdomain
   value   = local.vultr_machine.address
   type    = "CNAME"
   ttl     = 1
